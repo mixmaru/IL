e9cefd90682a0824e579e6dbcc65b12f012119fe
ryotashin_i <s_ryota@fc2.co.jp>
2012-11-13 16:11:13 +0900

    viewオブジェクト作成

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -11,38 +11,15 @@ class IndexController{
 	
 	public function __construct(){
 		//smarty
-		require_once("Smarty/Smarty.class.php");
-		$this->view = new Smarty();
-		$this->view->template_dir = $_SERVER['DOCUMENT_ROOT'].'/templates';
-		
-		//cssの読み込み準備
-		if(count($this->css_array) > 0){
-			$array = null;
-			for( $i=0; $i<count($this->css_array); $i++ ){
-				$array[$i] = '<link href="'.$this->css_array[$i].'" rel="stylesheet" type="text/css">';
-			}
-			$this->css = implode('', $array);
-		}
-		//jsの読み込み準備
-		if(count($this->js_array) > 0){
-			$array = null;
-			for( $i=0; $i<count($this->js_array); $i++ ){
-				$array[$i] = '<script type="text/javascript" src="'.$this->js_array[$i].'" ></script>';
-			}
-			$this->js = implode('', $array);
-		}
+		$this->view = new View($this->css_array, $this->js_array);
 	}
 	
 	public function indexAction(){
-		if($this->css){
-			$this->view->assign('css', $this->css);
-		}
-		if($this->js){
-			$this->view->assign('js', $this->js);
-		}
-		$this->view->assign('title', 'PicLip');
-		$this->view->assign('main_tpl', 'index.tpl');
-		$this->view->display('layout.tpl');
+		$conf = array(
+			'title' => 'PicLip',
+			'main_tpl' => 'index.tpl',
+		);
+		$this->view->display('layout.tpl', $conf);
 	}
 }
 ?>
---
+++ library/mvc/View.php
@@ -0,0 +1,54 @@
+<?php
+require_once("Smarty/Smarty.class.php");
+class View {
+	private $smarty;
+	private $css;
+	private $js;
+	
+	public function __construct($css_array, $js_array){
+		$this->smarty = new Smarty();
+		$this->smarty->template_dir = $_SERVER['DOCUMENT_ROOT'].'/templates/';
+		$this->smarty->compile_dir = $_SERVER['DOCUMENT_ROOT'].'/templates_c/';
+		$this->smarty->config_dir = $_SERVER['DOCUMENT_ROOT'].'/configs/';
+		$this->smarty->cache_dir = $_SERVER['DOCUMENT_ROOT'].'/cache/';
+		$this->smarty->caching = true;
+		
+		//cssの読み込み準備
+		$count = count($css_array);
+		if($count > 0){
+			$array = null;
+			for( $i=0; $i<$count; $i++ ){
+				$array[$i] = '<link href="'.$css_array[$i].'" rel="stylesheet" type="text/css">';
+			}
+			$this->css = implode('', $array);
+		}
+		//jsの読み込み準備
+		$count = count($js_array);
+		if($count > 0){
+			$array = null;
+			for( $i=0; $i<$count; $i++ ){
+				$array[$i] = '<script type="text/javascript" src="'.$js_array[$i].'" ></script>';
+			}
+			$this->js = implode('', $array);
+		}
+		
+	}
+	
+	public function display($base_tpl, $obj){
+		if($this->css){
+			$this->smarty->assign('css', $this->css);
+		}
+		if($this->js){
+			$this->smarty->assign('js', $this->js);
+		}
+		if($obj){
+			foreach($obj as $key => $value){
+				$this->smarty->assign($key, $value);
+			}
+		}
+		$this->smarty->display($base_tpl);
+	}
+	
+}
+
+?>
