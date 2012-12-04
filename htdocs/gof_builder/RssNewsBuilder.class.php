be4320fb30158d4ed4858bf0efd26c6820d0e22f
ryotashin_i <s_ryota@fc2.co.jp>
2012-12-04 14:35:56 +0900

    builderテスト

--- htdocs/gof_builder/RssNewsBuilder.class.php
+++ htdocs/gof_builder/RssNewsBuilder.class.php
@@ -5,9 +5,16 @@ require_once 'NewsBuilder.class.php';
 class RssNewsBuilder implements NewsBuilder{
 	public function parse($url){
 		$data = simplexml_load_file($url);
-		if($data = simplexml_load_file($url)){
-			
+		if($data === false){
+			throw new Exception('read data ['.htmlspecialchars($url, ENT_QUOTES).'] failed !');
 		}
+		
+		$list = array();
+		foreach($data->item as $item){
+			$dc = $item->children('http://purl.org/dc/elements/1.1/');
+			$list[] = new News($item->title, $item->link, $dc->date);
+		}
+		return $list;
 	}
 }
 ?>
