f0bb773d275a6d3fefdf62eae77fc0bf6bbdbe80
mixhomewin <mix.touch777@gmail.com>
2012-12-06 23:53:12 +0900

    Command作成中

---
+++ htdocs/gof_command/CopyCommand.class.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Command.class.php';
+require_once 'File.class.php';
+
+class CopyCommand implements Command{
+	private $file;
+	public function __construct(File $file){
+		$this->file = $file;
+	}
+	public function execute(){
+		$file = new File('copy_of_'.$this->tile->getName());
+		$file->create();
+	}
+}
+?>
