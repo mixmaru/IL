5937c701c2da1f0a09fcc4999799e7fad5c1e233
mixmac <mix.touch777@gmail.com>
2012-12-08 21:00:01 +0900

    flyweight追加

---
+++ htdocs/gof_flyweight/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'ItemFactory.class.php';
+
+function dumpData($data){
+	echo '<dl>';
+	foreach($data as $object){
+		echo '<dt>'.htmlspecialchars($object->getName(), ENT_QUOTES).'</dt>';
+		echo '<dd>商品番号：'.$object->getCode().'</dd>';
+		echo '<dd>\\'.number_format($object->getPrice()).'-</dd>';
+	}
+	echo '</dl>';
+}
+?>
+<?php
+
+$factory = ItemFactory::getInstance('data.txt');
+
+$items = array();
+$items[] = $factory->getItem('ABC0001');
+$items[] = $factory->getItem('ABC0002');
+$items[] = $factory->getItem('ABC0003');
+
+dumpData($items);
+
+if($items[0] === $factory->getItem('ABC0001')){
+	echo '同一のオブジェクトです';
+}else{
+	echo '同一のオブジェクトではありません';
+}
+
+?>
