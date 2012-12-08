7b5e11137c926e66960477a68633e028fe6223d4
mixmac <mix.touch777@gmail.com>
2012-12-08 15:05:29 +0900

    composite作成

---
+++ htdocs/gof_composite/Employee.class.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'OrganizationEntry.class.php';
+
+class Employee extends OrganizationEntry{
+
+		public function __construct($code, $name) {
+			parent::__construct($code, $name);
+		}
+		
+		public function add(OrganizationEntry $entry){
+			throw Exception('method not allowed');
+		}
+}
+
+?>
