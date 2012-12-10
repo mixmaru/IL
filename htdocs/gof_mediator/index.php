aa9c5dc16cbc91d8a52fa7b1ce178c1bd531c83e
ryotashin_i <s_ryota@fc2.co.jp>
2012-12-10 18:48:57 +0900

    gof_mediator作成

---
+++ htdocs/gof_mediator/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'Chatroom.class.php';
+require_once 'User.class.php';
+
+$chatroom = new Chatroom();
+
+$sasaki = new User("佐々木");
+$suzuki = new User("鈴木");
+$yoshida = new User("吉田");
+$kawamura = new User("川村");
+$tajima = new User("田島");
+
+$chatroom->login($sasaki);
+$chatroom->login($suzuki);
+$chatroom->login($yoshida);
+$chatroom->login($kawamura);
+$chatroom->login($tajima);
+
+$sasaki->sendMessage('鈴木', '来週の予定は？');
+$suzuki->sendMessage('川村', '秘密です');
+$yoshida->sendMessage('萩原', 'お元気ですか？');
+$tajima->sendMessage('佐々木', 'お邪魔してます？');
+$kawamura->sendMessage('吉田', '私事で恐縮ですが・・・');
+?>
