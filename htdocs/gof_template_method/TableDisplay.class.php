ebf07d5e18d7435730f52e1a2fdd847049da568a
ryotashin_i <s_ryota@fc2.co.jp>
2012-12-11 14:58:15 +0900

    テンプレートメソッドパターン完成

--- htdocs/gof_template_method/TableDisplay.class.php
+++ htdocs/gof_template_method/TableDisplay.class.php
@@ -1,3 +1,22 @@
 <?php
-reqire_once 'Abstre'
+require_once 'AbstractDisplay.class.php';
+
+class TableDisplay extends AbstractDisplay{
+	protected function displayBody() {
+		echo '<table border="1" cellpadding="2" cellspacing="2">';
+	}
+
+	protected function displayFooter() {
+		foreach($this->getData() as $key => $value){
+			echo '<tr>';
+			echo '<th>'.$key.'</td>';
+			echo '<td>'.$value.'</td>';
+			echo '</tr>';
+		}
+	}
+
+	protected function displayHeader() {
+		echo '</table>';
+	}
+}
 ?>
