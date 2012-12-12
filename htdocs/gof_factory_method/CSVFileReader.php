d3da79f1b0cd17dd457535ba100cbab7489825b0
ryotashin_i <s_ryota@fc2.co.jp>
2012-12-12 10:06:01 +0900

    factorymethod練習中

---
+++ htdocs/gof_factory_method/CSVFileReader.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Reader.class.php';
+
+class CSVFileReader implements Reader{
+	
+	private $filename;
+	
+	public function display() {
+		
+	}
+
+	public function read() {
+		
+	}
+}
+?>
